92192da11988e50292316b396dfbf96d936709f4
Thiago Jedi <thiagoa7@gmail.com>
2024-03-25 22:13:04 -0300

    Clearing form after adding block

--- src/components/block-form.tsx
+++ src/components/block-form.tsx
@@ -1,17 +1,25 @@
 import { useNewBlockMutation } from "../hooks";
+import { useState } from "preact/hooks";
 
 export const BlockForm = () => {
   const { trigger } = useNewBlockMutation();
+  const [value, setValue] = useState<string>();
   return (
     <section>
       <h2>Add new Block:</h2>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          trigger(new FormData(e.currentTarget));
+          trigger(new FormData(e.currentTarget)).then(() => setValue(""));
         }}
       >
-        <input type="text" name="domain" />
+        <input
+          type="text"
+          name="domain"
+          required
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+        />
         <input type="submit" value="Add Block" />
       </form>
     </section>
